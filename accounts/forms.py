4ee71597488aeee7dea2430a79e7df11f5435011
EternalTLD <shvejkin01@mail.ru>
2023-08-14 23:55:22 +0300

    add sigup

---
+++ accounts/forms.py
@@ -0,0 +1,49 @@
+from typing import Any, Dict
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+User = get_user_model()
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        max_length=25, 
+        label='Введите имя пользователя', 
+        help_text='Максимальная длинна - 25 символов. Имя не должно сожержать пробелов.'
+    )
+    email = forms.EmailField(label='Введите email')
+    password1 = forms.CharField(label='Введите пароль', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+    
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username):
+            raise forms.ValidationError('Пользователь с таким именем уже зарегистрирован.')
+        if ' ' in username:
+            raise forms.ValidationError('Имя не должно содержать пробелов.')
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email):
+            raise forms.ValidationError('Пользователь с таким email адресом уже зарегистрирован.')
+        return email
+    
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('Пароли на совпадают.')
+        return password2
+    
+    def save(self, commit=True) -> Any:
+        user = super(UserCreationForm, self).save(commit=False)
+        user.username = self.cleaned_data.get('username')
+        user.email = self.cleaned_data.get('email')
+        if commit:
+            user.save()
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
+from typing import Any
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from .forms import SignUpForm
+
+
+User = get_user_model()
+
+class SignUpView(CreateView):
+    model = User
+    form_class = SignUpForm
+    template_name = 'registration/signup.html'
+
+    def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        user_form = SignUpForm(request.POST)
+
+        if user_form.is_valid():
+            user = user_form.save(commit=False)
+            password = user_form.cleaned_data.get('password1')
+            user.set_password(password)
+            user.save()
+            return HttpResponseRedirect(reverse('accounts:login'))
