28fc0c41bf6e8b5f3d808ad2cedcaa8eea5feb7d
EternalTLD <shvejkin01@mail.ru>
2023-08-15 19:25:55 +0300

    add auth urls

--- config/settings.py
+++ config/settings.py
@@ -131,3 +131,13 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#LOGIN_REDIRECT_URL = 'messenger:home'
+
+LOGIN_REDIRECT_URL = 'accounts:home'
+
+LOGIN_URL = 'accounts:login'
+
+LOGOUT_URL = 'accounts:logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
