88de7f507b2e5c0be7764b148302c3c357ffc6f9
EternalTLD <shvejkin01@mail.ru>
2023-08-15 19:25:18 +0300

    add auth urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,52 @@
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
+from django.contrib.auth.views import (LoginView, LogoutView, PasswordChangeView,
+                                       PasswordChangeDoneView, PasswordResetView,
+                                       PasswordResetDoneView, PasswordResetConfirmView,
+                                       PasswordResetCompleteView)
 
-from .views import SignUpView
+from .views import SignUpView, home
 
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path('home/', home, name='home'),
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('', include('django.contrib.auth.urls'))
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path(
+        "password_change/", 
+        PasswordChangeView.as_view(
+            success_url=reverse_lazy('accounts:password_change_done')
+        ), 
+        name="password_change"
+    ),
+    path(
+        "password_change/done/",
+        PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
+    path(
+        "password_reset/", 
+        PasswordResetView.as_view(
+            success_url=reverse_lazy('accounts:password_reset_done')
+        ), 
+        name="password_reset"),
+    path(
+        "password_reset/done/",
+        PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(
+            success_url = reverse_lazy('accounts:password_reset_complete'),
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ]
