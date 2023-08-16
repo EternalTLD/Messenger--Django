be134b6340920f7ab054f9160a93b02bfd85b7a7
EternalTLD <shvejkin01@mail.ru>
2023-08-16 19:37:15 +0300

    add profile detail

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+
+User = get_user_model()
+
+class ProfileDetailView(DetailView):
+    template_name = 'profiles/profile_detail.html'
+    model = User
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+    context_object_name = 'user'
