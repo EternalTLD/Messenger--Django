31a4a266a08eb239c5e3009990091e03ce3242fc
EternalTLD <shvejkin01@mail.ru>
2023-08-17 00:42:03 +0300

    init friends app

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('profiles/', include('profiles.urls', namespace='profiles')),
+    path('friends/', include('friends.urls', namespace='friends')),
     path('', include('messenger.urls', namespace='messenger'))
 ]
