88795a1bbb147fb38c77c57541203b81d6f8318c
EternalTLD <shvejkin01@mail.ru>
2023-09-04 19:03:27 +0300

    add user search

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,5 +6,6 @@ app_name = 'profiles'
 
 urlpatterns = [
     path('edit/', views.ProfileEditView.as_view(), name='profile_edit'),
+    path('user_search', views.user_search_view, name='user_search'),
     path('<slug:username>/', views.ProfileDetailView.as_view(), name='profile_detail'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,13 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from django.contrib.auth import get_user_model
 
 from .forms import UserEditForm, ProfileEditForm
+from friends.models import Friend
 
 
 User = get_user_model()
@@ -37,3 +41,16 @@ class ProfileEditView(View):
             profile_form.save()
         context = {'user_form': user_form, 'profile_form': profile_form}
         return render(request, 'profiles/profile_edit.html', context)
+    
+def user_search_view(request, *args, **kwargs):
+    users = []
+
+    if request.method == 'GET':
+        query = request.GET.get('query')
+        auth_user = request.user
+        search_result = User.objects.filter(username__icontains=query).exclude(username=auth_user.username)
+        for user in search_result:
+            if not Friend.objects.are_friends(user, auth_user):
+                users.append(user)
+
+    return render(request, 'profiles/search_user.html', {'users': users})
