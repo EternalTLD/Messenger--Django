f4ae56784410d707c92bb51c4f70ee23d65c1fd1
EternalTLD <shvejkin01@mail.ru>
2023-09-10 22:20:33 +0300

    add accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, reverse_lazy
-from django.contrib.auth.views import (LoginView, LogoutView, PasswordChangeView,
-                                       PasswordChangeDoneView, PasswordResetView,
-                                       PasswordResetDoneView, PasswordResetConfirmView,
-                                       PasswordResetCompleteView)
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView, 
+    PasswordChangeView,
+    PasswordChangeDoneView
+)
 
 from .views import SignUpView
 
@@ -24,28 +26,5 @@ urlpatterns = [
         "password_change/done/",
         PasswordChangeDoneView.as_view(),
         name="password_change_done",
-    ),
-    path(
-        "password_reset/", 
-        PasswordResetView.as_view(
-            success_url=reverse_lazy('accounts:password_reset_done')
-        ), 
-        name="password_reset"),
-    path(
-        "password_reset/done/",
-        PasswordResetDoneView.as_view(),
-        name="password_reset_done",
-    ),
-    path(
-        "reset/<uidb64>/<token>/",
-        PasswordResetConfirmView.as_view(
-            success_url = reverse_lazy('accounts:password_reset_complete'),
-        ),
-        name="password_reset_confirm",
-    ),
-    path(
-        "reset/done/",
-        PasswordResetCompleteView.as_view(),
-        name="password_reset_complete",
     ),
 ]
