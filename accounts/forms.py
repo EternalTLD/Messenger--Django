fcf9f1a5d7bc80d9192766c6cc43e1cacd0d0909
EternalTLD <shvejkin01@mail.ru>
2023-09-10 22:20:01 +0300

    rework signup form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,19 +1,19 @@
-from typing import Any, Dict
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+
 
 User = get_user_model()
 
 class SignUpForm(UserCreationForm):
     username = forms.CharField(
-        max_length=25, 
-        label='Введите имя пользователя', 
-        help_text='Максимальная длинна - 25 символов. Имя не должно сожержать пробелов.'
+        max_length=25,
+        label='Enter username',
+        help_text='Maximum length is 25 characters. The username must not contain spaces.',
     )
-    email = forms.EmailField(label='Введите email')
-    password1 = forms.CharField(label='Введите пароль', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
+    email = forms.EmailField(label='Enter email', widget=forms.EmailInput)
+    password1 = forms.CharField(label='Enter password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
 
     class Meta:
         model = User
@@ -21,31 +21,34 @@ class SignUpForm(UserCreationForm):
     
     def clean_username(self):
         username = self.cleaned_data.get('username')
-        if User.objects.filter(username=username):
-            raise forms.ValidationError('Пользователь с таким именем уже зарегистрирован.')
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('User with this username is already exists.')
+        
         if ' ' in username:
-            raise forms.ValidationError('Имя не должно содержать пробелов.')
+            raise forms.ValidationError('Username must not contain spaces.')
+        
         return username
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
-        if User.objects.filter(email=email):
-            raise forms.ValidationError('Пользователь с таким email адресом уже зарегистрирован.')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('User with this email is already exists.')
+        
         return email
     
     def clean_password2(self):
         password1 = self.cleaned_data.get('password1')
         password2 = self.cleaned_data.get('password2')
         if password1 and password2 and password1 != password2:
-            raise forms.ValidationError('Пароли на совпадают.')
+            raise forms.ValidationError('Password mismatch.')
+        
         return password2
     
-    def save(self, commit=True) -> Any:
+    def save(self, commit=True):
         user = super(UserCreationForm, self).save(commit=False)
-        user.username = self.cleaned_data.get('username')
-        user.email = self.cleaned_data.get('email')
         password = self.cleaned_data.get('password1')
         user.set_password(password)
         if commit:
             user.save()
+
         return user
