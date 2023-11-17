4dee48a23f5a2f5c9defbb29b2091b9255dfbb35
EternalTLD <shvejkin01@mail.ru>
2023-11-17 16:00:28 +0300

    Added json response

--- friends/views.py
+++ friends/views.py
@@ -1,24 +1,10 @@
 import json
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 
 from .models import Friend, FriendshipRequest
-
-
-User = get_user_model()
-
-
-@login_required
-def add_friend_view(request):
-    if request.method == "POST":
-        user_id = json.loads(request.body)
-        to_user = User.objects.get(id=user_id)
-        from_user = request.user
-        Friend.objects.add_friend(from_user=from_user, to_user=to_user)
-
-    return JsonResponse({"Status": "Added"})
 
 
 @login_required
@@ -48,17 +34,6 @@ def reject_request_view(request):
 
 
 @login_required
-def remove_friend_view(request):
-    if request.method == "POST":
-        friend_id = json.loads(request.body)
-        to_user = User.objects.get(id=friend_id)
-        from_user = request.user
-        Friend.objects.remove_friend(from_user=from_user, to_user=to_user)
-
-    return JsonResponse({"Status": "Removed"})
-
-
-@login_required
 def unaccepted_requests_view(request):
     user = request.user
     requests = Friend.objects.unaccepted_requests(user=user)
@@ -78,6 +53,28 @@ def rejected_requests_view(request):
     return render(
         request, "friends/rejected_requests.html", {"user": user, "requests": requests}
     )
+
+
+@login_required
+def add_friend_view(request):
+    if request.method == "POST":
+        user_id = json.loads(request.body)
+        to_user = get_user_model().objects.get(id=user_id)
+        from_user = request.user
+        Friend.objects.add_friend(from_user=from_user, to_user=to_user)
+
+    return JsonResponse({"Status": "Added"})
+
+
+@login_required
+def remove_friend_view(request):
+    if request.method == "POST":
+        friend_id = json.loads(request.body)
+        to_user = get_user_model().objects.get(id=friend_id)
+        from_user = request.user
+        Friend.objects.remove_friend(from_user=from_user, to_user=to_user)
+
+    return JsonResponse({"Status": "Removed"})
 
 
 @login_required
