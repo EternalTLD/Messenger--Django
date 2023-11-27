fee3f2eb47d9fc6856fc9c53dd2cca61ab0c0bfb
EternalTLD <shvejkin01@mail.ru>
2023-11-27 19:25:08 +0300

    Added group room create and update

--- messenger/forms.py
+++ messenger/forms.py
@@ -1,9 +1,18 @@
+from typing import Any
 from django import forms
+from django.contrib.auth import get_user_model
 
 from .models import Room
 
 
 class GroupRoomCreateForm(forms.ModelForm):
+
     class Meta:
         model = Room
         fields = ["name", "participants"]
+
+    def __init__(self, user, *args: Any, **kwargs: Any) -> None:
+        super(GroupRoomCreateForm, self).__init__(*args, **kwargs)
+        self.fields['participants'].queryset = get_user_model().objects.filter(
+            id__in=user.friends.values_list("to_user", flat=True)
+        )
--- messenger/views.py
+++ messenger/views.py
@@ -1,17 +1,13 @@
 from typing import Any
-from django.db.models.query import QuerySet
-from django.forms.models import BaseModelForm
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views import generic
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
-from django.db.models import Q
+from django.db.models import Q, QuerySet
 
 from .models import Room
-from .services import get_direct_room_title
+from .forms import GroupRoomCreateForm
 
 
 @login_required
@@ -19,32 +15,31 @@ def index(request):
     return redirect("profiles:profile_detail", username=request.user.username)
 
 
-class DirectRoomListView(ListView):
+class DirectRoomListView(generic.ListView):
     template_name = "messenger/room_list.html"
     context_object_name = "room_list"
+    model = Room
 
     def get_queryset(self) -> QuerySet[Room]:
         user = self.request.user
-        room_list = Room.objects.filter(
+        room_list = self.model.objects.filter(
             participants__in=[user.id], room_type="D"
         ).distinct()
         return room_list
 
 
-class DirectRoomDetailView(DetailView):
+class DirectRoomDetailView(generic.DetailView):
     template_name = "messenger/room.html"
 
     def get(self, request, *args: Any, **kwargs: Any) -> HttpResponse:
-        room_name = self.kwargs.get("room_name")
-        room = get_object_or_404(Room, name=room_name, room_type="D")
-        room_title = get_direct_room_title(request.user.username, room_name)
-
+        room = get_object_or_404(Room, name=self.kwargs.get("room_name"), room_type="D")
+        room_title = room.participants.exclude(id=request.user.id).first()
         return render(
-            request, "messenger/room.html", {"room": room, "room_title": room_title}
+            request, self.template_name, {"room": room, "room_title": room_title}
         )
 
 
-class GroupRoomListView(ListView):
+class GroupRoomListView(generic.ListView):
     template_name = "messenger/room_list.html"
     context_object_name = "room_list"
 
@@ -56,37 +51,47 @@ class GroupRoomListView(ListView):
         return room_list
 
 
-class GroupRoomDetailView(DetailView):
-    template_name = "messenger/group_room.html"
+class GroupRoomDetailView(generic.DetailView):
+    template_name = "messenger/room.html"
 
     def get(self, request, *args: Any, **kwargs: Any) -> HttpResponse:
         room = get_object_or_404(Room, name=self.kwargs.get("room_name"), room_type="G")
-
         return render(
-            request, "messenger/room.html", {"room": room, "room_title": room.name}
+            request, self.template_name, {"room": room, "room_title": room.name}
         )
 
 
-class GroupRoomCreateView(CreateView):
+class GroupRoomCreateView(generic.CreateView):
     model = Room
-    fields = ["name", "participants"]
+    form_class = GroupRoomCreateForm
     template_name = "messenger/create_group_room.html"
-    success_url = reverse_lazy("messenger:room_list")
+    
+    def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        form = self.get_form()
+        if form.is_valid():
+            room = form.save(commit=False)
+            room.admin = request.user
+            room.room_type = "G"
+            room.save()
+            form.save_m2m()
+            return redirect("messenger:group_room", room_name=room.name)
+        return render(request, self.template_name, {"form": form})
+    
+    def get_form_kwargs(self) -> dict[str, Any]:
+        kwargs = super().get_form_kwargs()
+        kwargs.update({"user": self.request.user})
+        return kwargs
 
-    def form_valid(self, form: BaseModelForm) -> HttpResponse:
-        room = form.save(commit=False)
-        room.admin = self.request.user
-        room.room_type = "G"
-        room.save()
-        return super().form_valid(form)
 
-
-class GroupRoomUpdateView(UpdateView):
+class GroupRoomUpdateView(generic.UpdateView):
     model = Room
-    fields = ["name", "participants"]
+    form_class = GroupRoomCreateForm
     template_name = "messenger/update_group_room.html"
 
+    def get_success_url(self) -> str:
+        return redirect("messenger:group_room", room_name=self.kwargs.pop("room_name"))
 
-class GroupRoomDeleteView(DeleteView):
+
+class GroupRoomDeleteView(generic.DeleteView):
     model = Room
     success_url = reverse_lazy("messenger:room_list")
