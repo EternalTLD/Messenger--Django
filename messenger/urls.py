0f7681c9b10e173ac702ec9bac395ef909aa1011
EternalTLD <shvejkin01@mail.ru>
2023-11-27 19:25:53 +0300

    Updated urls

--- messenger/urls.py
+++ messenger/urls.py
@@ -5,13 +5,28 @@ app_name = "messenger"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("room_list/", views.GroupRoomListView.as_view(), name="room_list"),
+    
     path(
-        "direct/<str:username>/",
+        "direct_room_list/", 
+        views.DirectRoomListView.as_view(), 
+        name="direct_room_list"
+    ),
+    path(
+        "direct/<str:room_name>/",
         views.DirectRoomDetailView.as_view(),
         name="direct_room",
     ),
-    path("group/create/", views.GroupRoomCreateView.as_view(), name="group_create"),
+
+    path(
+        "group_room_list/", 
+        views.GroupRoomListView.as_view(), 
+        name="group_room_list"
+    ),
+    path(
+        "group/create/", 
+        views.GroupRoomCreateView.as_view(), 
+        name="group_create"
+    ),
     path(
         "group/<int:id>/update/",
         views.GroupRoomUpdateView.as_view(),
@@ -23,6 +38,8 @@ urlpatterns = [
         name="group_delete",
     ),
     path(
-        "group/<str:room_name>/", views.GroupRoomDetailView.as_view(), name="group_room"
+        "group/<str:room_name>/", 
+        views.GroupRoomDetailView.as_view(), 
+        name="group_room"
     ),
 ]
