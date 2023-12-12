a5ab5e88cedccffd8861ea0d08e1e99bcd36c135
EternalTLD <shvejkin01@mail.ru>
2023-12-12 17:56:02 +0300

    Refactored models

--- friends/models.py
+++ friends/models.py
@@ -1,8 +1,95 @@
+from __future__ import annotations
+
 from django.db import models
 from django.db.models import QuerySet
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 from django.conf import settings
+
+
+class FriendshipManager(models.Manager):
+    """Friendship manager"""
+
+    def friends(self, user) -> QuerySet[Friend]:
+        """Return a QuerySet of user friends"""
+        friends = Friend.objects.filter(to_user=user).select_related("from_user")
+        return friends
+
+    def received_requests(self, user) -> QuerySet[FriendshipRequest]:
+        """Return a QuerySet of friendship requests received by user"""
+        requests = FriendshipRequest.objects.filter(to_user=user).select_related(
+            "from_user", "to_user"
+        )
+        return requests
+
+    def sent_requests(self, user) -> QuerySet[FriendshipRequest]:
+        """Return a QuerySet of friendship requests sent by user"""
+        requests = FriendshipRequest.objects.filter(from_user=user).select_related(
+            "from_user", "to_user"
+        )
+        return requests
+
+    def unaccepted_requests(self, user) -> QuerySet[FriendshipRequest]:
+        """Return a QuerySet of requests that haven't been accepted"""
+        requests = FriendshipRequest.objects.filter(
+            to_user=user, rejected_at__isnull=True
+        ).select_related("from_user", "to_user")
+        return requests
+
+    def rejected_requests(self, user) -> QuerySet[FriendshipRequest]:
+        """Return a QuerySet of friendship requests rejected by user"""
+        requests = FriendshipRequest.objects.filter(
+            to_user=user,
+            rejected_at__isnull=False,
+        ).select_related("from_user", "to_user")
+        return requests
+
+    def add_friend(self, from_user, to_user) -> FriendshipRequest:
+        """Create a friendship request"""
+        if from_user == to_user:
+            raise ValidationError("You can't send a friendship request to yourself")
+
+        if self.request_already_sent(from_user=from_user, to_user=to_user):
+            raise ValidationError("Friendship request has already sent")
+
+        if self.request_already_received(from_user=to_user, to_user=from_user):
+            raise ValidationError("Friendship request has already received")
+
+        if self.are_friends(from_user, to_user):
+            raise ValidationError(f"Users are already friends")
+
+        request = FriendshipRequest.objects.create(
+            from_user=from_user,
+            to_user=to_user,
+        )
+        request.save()
+        return request
+
+    def remove_friend(self, from_user, to_user) -> Friend:
+        """Remove friend"""
+        return Friend.objects.filter(
+            models.Q(from_user=from_user, to_user=to_user)
+            | models.Q(from_user=to_user, to_user=from_user)
+        ).delete()
+
+    def request_already_sent(self, from_user, to_user) -> bool:
+        """Return True if user has already sent request to another user otherwise False"""
+        return FriendshipRequest.objects.filter(
+            from_user=from_user, to_user=to_user
+        ).exists()
+
+    def request_already_received(self, from_user, to_user) -> bool:
+        """Return True if user has already received request from another user otherwise False"""
+        return FriendshipRequest.objects.filter(
+            from_user=to_user, to_user=from_user
+        ).exists()
+
+    def are_friends(self, user1, user2) -> bool:
+        """Return True if users are friends otherwise False"""
+        return Friend.objects.filter(
+            models.Q(from_user=user1, to_user=user2)
+            | models.Q(from_user=user2, to_user=user1)
+        ).exists()
 
 
 class FriendshipRequest(models.Model):
@@ -48,92 +135,8 @@ class FriendshipRequest(models.Model):
         return True
 
 
-class FriendshipManager(models.Manager):
-    """Friendship manager"""
-
-    def friends(self, user) -> QuerySet:
-        """Return a QuerySet of user friends"""
-        friends = Friend.objects.filter(to_user=user).select_related("from_user")
-        return friends
-
-    def received_requests(self, user) -> QuerySet:
-        """Return a QuerySet of friendship requests received by user"""
-        requests = FriendshipRequest.objects.filter(to_user=user).select_related(
-            "from_user", "to_user"
-        )
-        return requests
-
-    def sent_requests(self, user) -> QuerySet:
-        """Return a QuerySet of friendship requests sent by user"""
-        requests = FriendshipRequest.objects.filter(from_user=user).select_related(
-            "from_user", "to_user"
-        )
-        return requests
-
-    def unaccepted_requests(self, user) -> QuerySet:
-        """Return a QuerySet of requests that haven't been accepted"""
-        requests = FriendshipRequest.objects.filter(
-            to_user=user, rejected_at__isnull=True
-        ).select_related("from_user", "to_user")
-        return requests
-
-    def rejected_requests(self, user) -> QuerySet:
-        """Return a QuerySet of friendship requests rejected by user"""
-        requests = FriendshipRequest.objects.filter(
-            to_user=user,
-            rejected_at__isnull=False,
-        ).select_related("from_user", "to_user")
-        return requests
-
-    def add_friend(self, from_user, to_user) -> FriendshipRequest:
-        """Create a friendship request"""
-        if from_user == to_user:
-            raise ValidationError("You can't send a friendship request to yourself")
-
-        if self.request_already_sent(from_user=from_user, to_user=to_user):
-            raise ValidationError("Friendship request has already sent")
-
-        if self.request_already_received(from_user=to_user, to_user=from_user):
-            raise ValidationError("Friendship request has already received")
-
-        if self.are_friends(from_user, to_user):
-            raise ValidationError(f"Users are already friends")
-
-        request = FriendshipRequest.objects.create(
-            from_user=from_user,
-            to_user=to_user,
-        )
-        request.save()
-        return request
-
-    def remove_friend(self, from_user, to_user) -> None:
-        """Remove friend"""
-        return Friend.objects.filter(
-            models.Q(from_user=from_user, to_user=to_user)
-            | models.Q(from_user=to_user, to_user=from_user)
-        ).delete()
-
-    def request_already_sent(self, from_user, to_user) -> bool:
-        """Return True if user has already sent request to another user otherwise False"""
-        return FriendshipRequest.objects.filter(
-            from_user=from_user, to_user=to_user
-        ).exists()
-
-    def request_already_received(self, from_user, to_user) -> bool:
-        """Return True if user has already received request from another user otherwise False"""
-        return FriendshipRequest.objects.filter(
-            from_user=to_user, to_user=from_user
-        ).exists()
-
-    def are_friends(self, user1, user2) -> bool:
-        """Return True if users are friends otherwise False"""
-        return Friend.objects.filter(
-            models.Q(from_user=user1, to_user=user2)
-            | models.Q(from_user=user2, to_user=user1)
-        ).exists()
-
-
 class Friend(models.Model):
+    """Friend model"""
     from_user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
