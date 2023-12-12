d9c846e3e99a24fad3c167c8b9fcd1208d6e4320
EternalTLD <shvejkin01@mail.ru>
2023-12-13 00:00:56 +0300

    Added admin profiles

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,9 +1,18 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
 
 from .models import Profile
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ["user", "profile_image", "phone", "bio"]
-    raw_id_fields = ["user"]
+    list_display = ("user", "phone", "country", "city", "status", "last_seen")
+    search_fields = ("user__username", "country", "city")
+    list_filter = ("last_seen",)
+    readonly_fields = ("status", "last_seen")
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Profile]:
+        qs = super().get_queryset(request)
+        return qs.select_related("user")
