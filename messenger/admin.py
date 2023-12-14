e202c0e62917f8eeb131416b8a5f95de558a439e
EternalTLD <shvejkin01@mail.ru>
2023-12-14 15:40:43 +0300

    Added rooms and messages

--- messenger/admin.py
+++ messenger/admin.py
@@ -5,9 +5,17 @@ from .models import Room, Message
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    list_display = ["name"]
+    list_display = ("name", "admin", "room_type", "members_count")
+    search_fields = ("name",)
+    list_filter = ("room_type",)
 
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ["author", "content", "room"]
+    list_display = ("author", "short_content", "timestamp", "room")
+    search_fields = ("author", "content")
+    list_filter = ("timestamp",)
+
+    @admin.display(description="Content")
+    def short_content(self, obj):
+        return (obj.content[:30] + "...") if len(obj.content) > 30 else obj.content
