92b981bb9da0821a46aad08388d71cfeb45fdf6e
EternalTLD <shvejkin01@mail.ru>
2023-12-14 15:41:37 +0300

    Added group room create form

--- messenger/forms.py
+++ messenger/forms.py
@@ -5,14 +5,16 @@ from django.contrib.auth import get_user_model
 from .models import Room
 
 
-class GroupRoomCreateForm(forms.ModelForm):
+User = get_user_model()
 
+
+class GroupRoomCreateForm(forms.ModelForm):
     class Meta:
         model = Room
         fields = ["name", "participants"]
 
     def __init__(self, user, *args: Any, **kwargs: Any) -> None:
         super(GroupRoomCreateForm, self).__init__(*args, **kwargs)
-        self.fields['participants'].queryset = get_user_model().objects.filter(
+        self.fields["participants"].queryset = User.objects.filter(
             id__in=user.friends.values_list("to_user", flat=True)
         )
