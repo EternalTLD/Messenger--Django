a629dca5729ff13dd6f17aad61227ec2f440e1d5
EternalTLD <shvejkin01@mail.ru>
2023-12-21 17:32:48 +0300

    Changed asgi for uvicorn

--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,20 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from messenger.routing import websocket_urlpatterns
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 django_asgi_app = get_asgi_application()
+
+
+from channels.auth import AuthMiddlewareStack
+from messenger.routing import websocket_urlpatterns
+
 
 application = ProtocolTypeRouter(
     {
